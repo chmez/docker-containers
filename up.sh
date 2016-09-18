43790c068e6a8f56cf89c7df82d5796946b7f9f5
chmez <gorbatuk.oleksandr@gmail.com>
2016-09-19 00:15:50 +0300

    Changed root accout password and set permissions for default site directory before clear cache.

--- up.sh
+++ up.sh
@@ -18,6 +18,7 @@ docker exec -it portfolio service apache2 start
 docker exec -it portfolio service mysql start
 
 docker exec -it portfolio chown -R www-data:www-data sites/default
-docker exec -it portfolio drush si portfolio --db-url=mysql://root:root@localhost:3306/alexgor_portfoli --account-name=alexgor_portfoli --account-pass=root --account-mail=chmez070@gmail.com --locale=uk --site-name=Portfolio --site-mail=chmez070@gmail.com -y
+docker exec -it portfolio drush si portfolio --db-url=mysql://root:root@localhost:3306/alexgor_portfoli --account-name=alexgor_portfoli --account-pass=394zqcdq --account-mail=chmez070@gmail.com --locale=uk --site-name=Portfolio --site-mail=chmez070@gmail.com -y
 
+docker exec -it portfolio chown -R www-data:www-data sites/default
 docker exec -it portfolio drush cr
