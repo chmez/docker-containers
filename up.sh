df8bdf1afa48ceee07c724126edb2a57492c2481
chmez <chmez070@gmail.com>
2016-09-28 19:22:58 +0300

    Deleted personal accesses.

--- up.sh
+++ up.sh
@@ -1,7 +1,5 @@
 #/bin/sh
 
-sudo rm -rf htdocs
-
 CONTAINER_ID=$(docker ps -a -q --filter="name=portfolio")
 
 if [ $CONTAINER_ID ]
@@ -18,7 +16,7 @@ docker exec -it portfolio service apache2 start
 docker exec -it portfolio service mysql start
 
 docker exec -it portfolio chown -R www-data:www-data sites/default
-docker exec -it portfolio drush si portfolio --db-url=mysql://root:root@localhost:3306/alexgor_portfoli --account-name=alexgor_portfoli --account-pass=394zqcdq --account-mail=chmez070@gmail.com --locale=uk --site-name=Portfolio --site-mail=chmez070@gmail.com -y
+docker exec -it portfolio drush si portfolio --db-url=mysql://root:root@localhost:3306/portfolio --account-name=root --account-pass=root --account-mail=test@gmail.com --locale=uk --site-name=Portfolio --site-mail=test@gmail.com -y
 
 docker exec -it portfolio chown -R www-data:www-data sites/default
 docker exec -it portfolio drush cr
